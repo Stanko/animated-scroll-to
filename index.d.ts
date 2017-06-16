3dccb2da3bc81685dca7f90ebab9a19f219e4751
idready <you_sombo@hotmail.fr>
2017-06-16 22:13:07 +0200

    TypeScript type definitions file

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for AnimateScrollTo 1.0.1
+// Project: https://github.com/Stanko/animated-scroll-to
+// Definitions by: Stanko <https://github.com/Stanko/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function animateScrollTo(desiredOffset: number):void;
+export default animateScrollTo;
