63e98c52d7eeb496b8fd2967a2a55e132d10778e
Stanko <stanko.tadic@gmail.com>
2019-11-04 08:26:43 +0800

    Removed lib from .gitignore

---
+++ lib/animated-scroll-to.d.ts
@@ -0,0 +1,16 @@
+declare type TCoords = [number | null, number | null];
+interface IOptions {
+    cancelOnUserAction?: boolean;
+    easing?: (t: number) => number;
+    elementToScroll?: Element | Window;
+    horizontalOffset?: number;
+    maxDuration?: number;
+    minDuration?: number;
+    passive?: boolean;
+    speed?: number;
+    verticalOffset?: number;
+}
+declare function animateScrollTo(y: number, userOptions?: IOptions): any;
+declare function animateScrollTo(coords: TCoords, userOptions?: IOptions): any;
+declare function animateScrollTo(scrollToElement: Element, userOptions?: IOptions): any;
+export default animateScrollTo;
